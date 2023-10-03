8df718531b5c3165a6ff72a6a80ea94ce740edad
Root99 <azizrezgui4@gmail.com>
2023-10-03 23:47:02 +0100

    removed fb from socials

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -63,21 +63,21 @@ const Skills = () => {
             cursor="pointer"
           />
         </Link>
-        <Link href="https://www.facebook.com/Rezguiaziz99/" isExternal>
-          <Icon
-            as={FiFacebook}
-            boxSize="30px"
-            opacity="0.7"
-            transform={"auto"}
-            transition="all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1) 0.1s"
-            _hover={{
-              translateY: "-5px",
-              opacity: 1,
-              color: "themeCyan",
-            }}
-            cursor="pointer"
-          />
-        </Link>
+        {/*<Link href="https://www.facebook.com/Rezguiaziz99/" isExternal>*/}
+        {/*  <Icon*/}
+        {/*    as={FiFacebook}*/}
+        {/*    boxSize="30px"*/}
+        {/*    opacity="0.7"*/}
+        {/*    transform={"auto"}*/}
+        {/*    transition="all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1) 0.1s"*/}
+        {/*    _hover={{*/}
+        {/*      translateY: "-5px",*/}
+        {/*      opacity: 1,*/}
+        {/*      color: "themeCyan",*/}
+        {/*    }}*/}
+        {/*    cursor="pointer"*/}
+        {/*  />*/}
+        {/*</Link>*/}
         <Link href="https://wa.me/+21692222079" isExternal>
           <Icon
             as={BsWhatsapp}
